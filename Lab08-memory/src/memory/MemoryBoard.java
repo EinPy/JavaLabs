3188d9174794d3174d75be16f67a4bf9dbb30877
Einar Bratthall <einar.bratthall@gmail.com>
2022-02-23 14:57:41 +0100

    added gitignore and completed lab 8

--- Lab08-memory/src/memory/MemoryBoard.java
+++ Lab08-memory/src/memory/MemoryBoard.java
@@ -1,50 +1,83 @@
 package memory;
 
+import java.util.Random;
 
 public class MemoryBoard {
-
+	private int size;
+	private MemoryCardImage[][] cards;
+	private String backN;
+	private String[] frontNms;
+	private static Random rand = new Random();
+	private boolean[][] facingU;
 	/** Skapar ett memorybräde med size * size kort. backFileName är filnamnet 
 	    för filen med baksidesbilden. Vektorn frontFileNames innehåller filnamnen 
 	    för frontbilderna. */
 	public MemoryBoard(int size, String backFileName, String[] frontFileNames) {
-
+		this.size = size;
+		this.cards = new MemoryCardImage[size][size];
+		this.backN = backFileName;
+		this.frontNms = frontFileNames;
+		createCards(backFileName,frontFileNames);
+		this.facingU = new boolean[size][size];
 	}
 
 	/* Skapar size * size / 2 st memorykortbilder.
 	   Placerar ut varje kort på två slumpmässiga ställen på spelplanen. */
-	private void createCards(String backFileName, String[] frontFileNames) {
-
+	private void createCards(String bfn, String[] ffn) {
+		int placed = 0;
+		for (int i = 0; i < ffn.length; i++) {
+			var img = new MemoryCardImage(ffn[i], bfn);
+			while (placed < 2) {
+				int r = rand.nextInt(size), c = rand.nextInt(size);
+				if (cards[r][c] == null) {
+					cards[r][c] = img;
+					placed++;
+				}
+			}
+			placed = 0;
+		}
 	}
 
 	/** Tar reda på brädets storlek. */
 	public int getSize() {
-		return 0;
+		return size;
 	}
 	
 	/** Hämtar den tvåsidiga bilden av kortet på rad r, kolonn c.
 	    Raderna och kolonnerna numreras från 0 och uppåt. */
 	public MemoryCardImage getCard(int r, int c) {
-		return null;
+		return cards[r][c];
 	}
 
 	/** Vänder kortet på rad r, kolonn c. */
 	public void turnCard(int r, int c) {
-
+		if(!facingU[r][c]) {
+			facingU[r][c] = true;
+		}else {
+			facingU[r][c] = false;
+		}
 	}
 	
 	/** Returnerar true om kortet r, c har framsidan upp. */
 	public boolean frontUp(int r, int c) {
-		return false;
+		return facingU[r][c];
 	}
 	
 	/** Returnerar true om det är samma kort på rad r1, kolonn c2 som på rad r2, 
 	    kolonn c2. */
 	public boolean same(int r1, int c1, int r2, int c2) {
-		return false;
+		return cards[r1][c1].equals(cards[r2][c2]);
 	}
 
 	/** Returnerar true om alla kort har framsidan upp. */
 	public boolean hasWon() {
-		return false;
+		for (int r = 0; r < size; r++) {
+			for (int c = 0; c < size; c++) {
+				if (! facingU[r][c]) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}	
 }
--- Lab08-memory/src/memory/MemoryGame.java
+++ Lab08-memory/src/memory/MemoryGame.java
@@ -1,11 +1,46 @@
 package memory;
 
+import se.lth.cs.pt.window.SimpleWindow;
+
 public class MemoryGame {
 	public static void main(String[] args) {
 		String[] frontFileNames = {"can.jpg", "flopsy_mopsy_cottontail.jpg",
 				"friends.jpg", "mother_ladybird.jpg", "mr_mcgregor.jpg",
 				"mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg" };
+		String back = "back.jpg";
 		
 		// Fyll i egen kod här
+		int size = 4;
+		var board = new MemoryBoard(size, back, frontFileNames);
+		var window = new MemoryWindow(board);
+		window.drawBoard();
+		boolean playing = true;
+		
+		int moves = 0;
+		while(playing) {
+			window.waitForMouseClick();
+			int r1 = window.getMouseRow();
+			int c1 = window.getMouseCol();
+			board.turnCard(r1,c1);
+			window.drawCard(r1,c1);
+			window.waitForMouseClick();
+			int r2 = window.getMouseRow();
+			int c2 = window.getMouseCol();
+			board.turnCard(r2,c2);
+			window.drawCard(r2,c2);
+			moves ++;
+			if (board.same(r1, c1, r2, c2)) {
+				if (board.hasWon()) {
+					System.out.print("CONGRATULATIONS, you won in" + moves + "moves");
+				}
+			}else {
+				SimpleWindow.delay(1000);
+				board.turnCard(r1,c1);
+				board.turnCard(r2,c2);
+				window.drawBoard();
+			}
+		}
+		
+		
 	}
 }
